e20aa2473fed73f5dcca7a0548203344e3e30dbe
Jan Kremen <jan@expan.do>
2019-01-31 14:51:39 +0100

    work on schema composition

--- src/decorators/resolver.ts
+++ src/decorators/resolver.ts
@@ -1,29 +1,14 @@
-import {
-    ClassType,getOrCreateResolver, getParamNames,
-    getPropertyGraphqlType,
-    mapArguments, TypeFn
-} from "../graphq-compose-typescript";
+import {buildResolver, ClassType, TypeFn} from "../graphq-compose-typescript";
 
 export class InstanceMissing extends Error {
     constructor(type: ClassType) {
         super(`Missing instance for ${type.name}.`);
     }
-
 }
 
 export function $resolver<T>(typeFn?: TypeFn): PropertyDecorator {
     return (target: T, propertyKey: string) => {
         const constructor = target.constructor as ClassType;
-        let resolver = getOrCreateResolver(constructor, propertyKey);
-        let graphqlType = getPropertyGraphqlType(constructor, propertyKey, typeFn);
-        resolver.setType(graphqlType);
-        resolver.setResolve(function (rp) {
-            const instance = rp.context && rp.context.instance;
-            if (!instance) {
-                throw new InstanceMissing(constructor);
-            }
-            let parameters = mapArguments(rp, getParamNames(constructor, propertyKey));
-            return instance[propertyKey](...parameters);
-        });
+        buildResolver(constructor, propertyKey, typeFn)
     };
 }
